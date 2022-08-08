06897a83736e474161f36e1fb3de78a72b65681c
sherifhelmyzayed <sherifhelmyzayed@gmail.com>
2022-08-08 11:37:15 +0200

    feat: hovering works on glb elements

--- src/Models/Apartments.js
+++ src/Models/Apartments.js
@@ -4,57 +4,41 @@ import { useGLTF } from "@react-three/drei";
 
 
 export function Model(props) {
-  const [hovered, setHover] = useState(null);
 
   const { nodes, materials } = useGLTF("/apartments.glb");
   const arrayOfObj = Object.entries(nodes).map((mesh) => ({ mesh })).slice(3, 20);
-  const hoverObj = Object.entries(nodes).map((mesh) => ({ id: mesh[1].uuid, hovered: false })).slice(3, 20);
-  // const [{ arrayOfObj, cycle }, set] = useState({ objects: [], cycle: 0 })
-
-  console.log(hoverObj);
-
-  let uuid = []
-
-  arrayOfObj.map(item => (
-    uuid.push({ id: item.mesh[1].uuid, hover: false })
-  ))
-
-  setHover(hoverObj)
-
-  
 
 
-  // setTimeout(() => {
-  //   setHover(uuid)
-  // }, 1000);
+  const Apartment = (props) => {
+    const [hovered, setHovered] = useState(false);
 
-  // useEffect(() => {
-  //   console.log(hovered);
-  //   return () => {
-  //     setHover(uuid)
-  //   }
-  // }, [uuid])
+    const { geometry, material } = props;
 
 
+    return (
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={geometry}
 
-  console.log(hovered);
+        onPointerOver={e => {
+          e.stopPropagation();
+          setHovered(true)
+        }}
+        onPointerOut={e => {
+          e.stopPropagation();
+          setHovered(false);
+        }}
 
-  const pointOut = (e, id) => {
-    e.stopPropagation()
-    console.log(id);
-
-    let arr = hovered
-
-    // arr.map(item => console.log(item))
-
+        onClick={e => {
+          e.stopPropagation();
+          console.log(geometry.boundingSphere.center);
+        }}
+      >
+        <meshStandardMaterial {...material} color={hovered ? "aquamarine" : "#d4d4d4"} />
+      </mesh>
+    )
   }
-
-  const pointOver = (e, id) => {
-    e.stopPropagation()
-    console.log(id);
-
-  }
-
 
 
 
@@ -71,128 +55,13 @@ export function Model(props) {
             scale={[1, 1, 0.03]}
           />
           {
-            arrayOfObj.map((item) => {
+            arrayOfObj.map(item => {
               return (
-                <mesh
-                  castShadow
-                  receiveShadow
-                  geometry={item.mesh[1].geometry}
-                  material={materials.GreyColor}
-                  material-color={hovered ? "aquamarine" : 'black'}
-
-                  onPointerOver={(e) => pointOver(e, item.mesh[1].uuid)}
-                  onPointerOut={(e) => pointOut(e, item.mesh[1].uuid)}>
-
-                  {/* onPointerOver={(e) => {
-                    e.stopPropagation()
-                    setHover((true))
-                  }}
-                  onPointerOut={(e) => {
-                    e.stopPropagation()
-                    setHover(false)
-                  }}> */}
-                </mesh>
-
+                <Apartment geometry={item.mesh[1].geometry} material = {materials.GreyColor}/>
               )
             })
           }
 
-          {/* <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim1.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim10.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim11.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim12.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim13.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim14.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim15.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim2.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim3.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim4.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim5.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim6.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim7.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim8.geometry}
-            material={materials.GreyColor}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Trim9.geometry}
-            material={materials.GreyColor}
-          /> */}
         </group>
       </group>
 
